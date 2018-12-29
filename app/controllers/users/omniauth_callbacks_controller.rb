31774f77250a497b9fd87097005bca8d93650131
lisset-orozco <liss612@gmail.com>
2018-12-28 23:16:36 -0600

    the improvements were applied, final code review part 1

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,22 +15,23 @@ module Users
 
     def custom_sign_up
       @user = User.from_omniauth(session['devise.auth'])
-      if @user.update(user_params)
-        sign_in_and_redirect(@user, event: :authentication)
-      else
-        render :edit
-      end
+      render :edit and return unless @user.update(user_params)
+      sign_in_and_redirect(@user, event: :authentication)
     end
 
     def failure
-      redirect_to new_user_session_path, notice: "Hubo un error con el login. Intenta de nuevo"
+      redirect_to(new_user_session_path, notice: "Hubo un error con el login. Intenta de nuevo")
     end
 
     private
 
     def user_params
       # Strong Params
-      params.require(:user).permit(:name, :username, :email)
+      params.require(:user).permit(
+        :name, 
+        :username, 
+        :email
+      )
     end
   end
 end
