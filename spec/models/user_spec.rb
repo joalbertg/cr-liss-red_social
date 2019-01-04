b3c3e64ac8bec025ccf4871b18f1055fdd755521
lisset-orozco <liss612@gmail.com>
2019-01-03 23:53:44 -0600

    unit tests with rspec for custom validation of regular expressions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe User, type: :model do
   it { should validate_uniqueness_of(:username) }
   it { should validate_length_of(:username).is_at_least(3).is_at_most(12) }
 
+  describe "#validate_username_regex" do
+    it "should not allow username with numbers at the beginning" do
+      user = User.new(email:'lisset2@gmail.com', password: '123456', username: '9asda')
+      expect(user.valid?).to be_falsy
+    end
+
+    it "should not contain special characters" do
+      user = User.new(email:'lisset2@gmail.com', password: '123456', username: 'liss**')
+      expect(user.valid?).to be_falsy
+    end
+  end
 end
