acaa92bacdf206e771ba2d85a6379bbc5ac19b0d
lisset-orozco <liss612@gmail.com>
2019-01-09 15:56:00 -0600

    add fields in the view and update the profile in the controller

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -2,11 +2,30 @@ class UsuariosController < ApplicationController
   before_action :set_user
   def show; end
 
-  def update; end
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user }
+        format.json { render :show }
+        format.js 
+      else
+        format.html { redirect_to @user, notice: @user.errors.full_messages }
+        format.html { render @user.errors }
+      end
+    end
+  end
 
   private
 
   def set_user
     @user = User.find_by_id(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, 
+                                  :last_name, 
+                                  :username, 
+                                  :email, 
+                                  :bio)
   end
 end
