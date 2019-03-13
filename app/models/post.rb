be312826e0add4d5f4e43d409551643fcb56d89e
lisset-orozco <liss612@gmail.com>
2019-03-13 14:21:01 -0600

    real-time posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,10 +13,31 @@
 class Post < ApplicationRecord
   belongs_to :user
   scope :recent, -> { order('created_at desc') }
+  after_create :send_to_action_cable
 
   def self.all_for_user(user)
     Post.where(user_id: user.id)
         .or(Post.where(user_id: user.friend_ids))
         .or(Post.where(user_id: user.user_ids))
+  end
+
+  private
+
+  def send_to_action_cable
+    data = { message: to_html, action: 'new_post' }
+
+    user.friend_ids.each do |friend_id|
+      ActionCable.server.broadcast 'some_channel', data
+    end
+
+    user.user_ids.each do |friend_id|
+      ActionCable.server.broadcast 'some_channel', data
+    end
+  end
+
+  def to_html
+    ApplicationController.renderer.render(partial: 'posts/post',
+                                          locals: { post: self })
+    # locals: local variables used by the partial
   end
 end
