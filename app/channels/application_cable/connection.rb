b0e55b1ecb9ae7cf9b40181bed24cf3b212be76e
lisset-orozco <liss612@gmail.com>
2019-03-13 14:56:25 -0600

    identify the current user in ActionCable using cookies with warden_hooks

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
+# frozen_string_literal: true
+
 module ApplicationCable
+  # connection cable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_user
+    end
+
+    def find_user
+      user_id = cookies.signed['user.id']
+      current_user = User.find_by_id(user_id)
+
+      return current_user if current_user
+
+      reject_unauthorized_connecction
+    end
   end
 end
